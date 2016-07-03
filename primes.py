03c6659bb8ab049050cb1dc82d58a01fa8898484
Vasily Gnuchev <gnuchev@gmail.com>
2016-07-04 02:45:39 +0300

    added some comments

--- primes.py
+++ primes.py
@@ -1,15 +1,20 @@
 import time
 
+#adding some color
 def prGreen(prt): print("\033[92m {}\033[00m" .format(prt))
 def prCyan(prt): print("\033[96m {}\033[00m" .format(prt))
 
 time1 = 0
+
+#waiting for input from user
 print(' Hi!')
 prGreen("I can calculate all the primes from 0 to n insanely fast")
 n = int(input(" Enter n ="))
 
+#go!
 start = time.time()
 
+#calculation
 a = list(range(n+1))
 a[1] = 0
 lst = [1]
@@ -22,15 +27,20 @@ while i <= n:
             a[j] = 0
     i += 1
 
+#calculation done! It's time to update time
 finish = time.time()
 time1 = start - finish
 
+#instant output and small delay with printing all numbers - helpful with huge numbers, like 100.000.000
 prGreen('.........\n .......\n .....\n ...\n .\nCalculation complete!!!')
 print('Prime numbers found:', len(lst), '\nTime to complete:', time1)
 print('Printing results:')
 time.sleep(2)
 
+#printing list of all prime numbers in range
 prCyan(lst)
+
+#and some fun facts
 print('')
 print('Prime numbers in range from 0 to', n, ':', len(lst))
 print('Sum of all prime numbers in range =', sum(lst))
